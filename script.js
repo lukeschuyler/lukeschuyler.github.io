62150deb793d803f4d54a4b01e8527366ac911aa
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-28 23:50:31 -0600

    re made navs, added scroll on click

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 var $name = $(".name-fade");
 
+$('.work-spans').hide();
+
 $(document).ready(function() {
-	$($name).fadeIn(3000);
+	$($name).fadeIn(2000);
+	setTimeout(function() {
+		$('#work1').fadeIn(2000);
+		setTimeout(function() {
+			$('#work2').fadeIn(2000);
+		}, 1200);
+		setTimeout(function() {
+			setTimeout(function() {
+			$('#work3').fadeIn(2000);
+		}, 2400);
+		})
+	}, 1500)
 });
 
+$('.s-icons').mouseover(function() {
+	$(this).addClass('hover-icon');
+	console.log('hey')
+})
 
-
-// $(document).ready(function typeLetters() {
-// 	for
-// });
+$('#about').click(function(e) {
+	$('html, body').animate({
+        scrollTop: $(".pic-container2").offset().top
+    }, 2000);
+});
