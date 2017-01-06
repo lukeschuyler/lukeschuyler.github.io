64b207025140871b3a9ced29417c68197c5494da
Luke Schuyler <lukeschuyler1@gmail.com>
2017-01-06 16:26:26 -0600

    added animatecss

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 var $name = $(".name-fade");
 
-$('.work-spans').hide();
+// $('#aboutView').hide();
 
-$(document).ready(function() {
-	$($name).fadeIn(2000);
-	setTimeout(function() {
-		$('#work1').fadeIn(2000);
-		setTimeout(function() {
-			$('#work2').fadeIn(2000);
-		}, 1200);
-		setTimeout(function() {
-			setTimeout(function() {
-			$('#work3').fadeIn(2000);
-		}, 2400);
-		})
-	}, 1500)
-});
+// $(document).ready(function() {
+// 	$($name).fadeIn(2000);
+// 	setTimeout(function() {
+// 		$('#work1').fadeIn(2000);
+// 		setTimeout(function() {
+// 			$('#work2').fadeIn(2000);
+// 		}, 1200);
+// 		setTimeout(function() {
+// 			setTimeout(function() {
+// 			$('#work3').fadeIn(2000);
+// 		}, 2400);
+// 		})
+// 	}, 1500)
+// });
 
-$('.s-icons').mouseover(function() {
-	$(this).addClass('hover-icon');
-	console.log('hey')
-})
+// $('.s-icons').mouseover(function() {
+// 	$(this).addClass('hover-icon');
+// 	console.log('hey')
+// })
 
-$('#about').click(function(e) {
-	$('html, body').animate({
-        scrollTop: $(".pic-container2").offset().top
-    }, 2000);
-});
+// $('#about').click(function(e) {
+// 	$('html, body').animate({
+//         scrollTop: $(".pic-container2").offset().top
+//     }, 2000);
+// });
