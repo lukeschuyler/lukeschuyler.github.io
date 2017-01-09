a95e529bebd69e7c6cc6c56615c550ab5aa04da9
Luke Schuyler <lukeschuyler1@gmail.com>
2017-01-08 19:17:01 -0600

    added button hides and shows

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 var $name = $(".name-fade");
 
-// $('#aboutView').hide();
+$('#aboutView').hide();
 
-// $(document).ready(function() {
-// 	$($name).fadeIn(2000);
-// 	setTimeout(function() {
-// 		$('#work1').fadeIn(2000);
-// 		setTimeout(function() {
-// 			$('#work2').fadeIn(2000);
-// 		}, 1200);
-// 		setTimeout(function() {
-// 			setTimeout(function() {
-// 			$('#work3').fadeIn(2000);
-// 		}, 2400);
-// 		})
-// 	}, 1500)
-// });
+$('#about').click(function(e) {
+	$('.banner').hide('slow');
+	setTimeout(function() {
+		$('.pic-container').hide('slow');
+		$('#aboutView').show('slow');
+	}, 500);
 
-// $('.s-icons').mouseover(function() {
-// 	$(this).addClass('hover-icon');
-// 	console.log('hey')
-// })
+})
 
-// $('#about').click(function(e) {
-// 	$('html, body').animate({
-//         scrollTop: $(".pic-container2").offset().top
-//     }, 2000);
-// });
+$(document).ready(function() {
+	$($name).fadeIn(2000);
+	setTimeout(function() {
+		$('#work1').fadeIn(2000);
+		setTimeout(function() {
+			$('#work2').fadeIn(2000);
+		}, 1200);
+		setTimeout(function() {
+			setTimeout(function() {
+			$('#work3').fadeIn(2000);
+		}, 2400);
+		})
+	}, 1500)
+});
+
+$('.link').mouseover(function() {
+	$(this).parent().addClass('pulse');
+})
+
+$('.link').mouseleave(function() {
+	$(this).parent().removeClass('pulse');
+})
+
+$('#about').click(function(e) {
+	$('html, body').animate({
+        scrollTop: $(".pic-container2").offset().top
+    }, 2000);
+});
