c404fe6b2a7828668d464c0532d1deb336afe6f6
Luke Schuyler <lukeschuyler1@gmail.com>
2017-01-08 23:06:31 -0600

    made new branch and redesigned

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
 	$($name).fadeIn(3000);
 });
 
-
-
-// $(document).ready(function typeLetters() {
-// 	for
+// $('a').click(function(e) {
+// 	e.preventDefault();
+// 	console.log(this)
+// 	$(this).addClass('selected');
+// 	$(this).siblings().removeClass('selected');
 // });
+
+$('.links').mouseover(function(e) {
+	$(this).addClass('hover');
+});
