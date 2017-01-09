6ac09b3ecdc88cf219585b78524a69ae7ebbda31
Luke Schuyler <lukeschuyler1@gmail.com>
2017-01-08 23:47:33 -0600

    added view clicks

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-var $name = $(".name-fade");
+function hideAll (view) {
+	$('#about').hide();
+	$('#music').hide();
+	$('#work').hide();
+	$('#contact').hide();
+	$('#front').hide();
+	$(view).show();
+};
 
-$(document).ready(function() {
-	$($name).fadeIn(3000);
+$('#about').hide();
+	$('#music').hide();
+	$('#work').hide();
+	$('#contact').hide();
+
+$('a').click(function(e) {
+	e.preventDefault();
+	console.log(this)
+	console.log($(this).siblings())
+	$(this).addClass('selected');
+	$(this).siblings().removeClass('selected');
 });
-
-// $('a').click(function(e) {
-// 	e.preventDefault();
-// 	console.log(this)
-// 	$(this).addClass('selected');
-// 	$(this).siblings().removeClass('selected');
-// });
 
 $('.links').mouseover(function(e) {
-	$(this).addClass('hover');
+	$(this).addClass('pulse');
 });
+
+$('.links').mouseleave(function(e) {
+	$(this).removeClass('pulse');
+});
+
+$('.icons').mouseover(function(e) {
+	$(this).addClass('pulse');
+});
+
+$('.icons').mouseleave(function(e) {
+	$(this).removeClass('pulse');
+});
+
+$('.home').click(function() {
+	hideAll($('#front'));
+	// $('#front').show();
+});
+
+$('.about').click(function() {
+	hideAll($('#about'));
+});
+
+$('.work').click(function() {
+	hideAll($('#work'));
+});
+
+$('.contact').click(function() {
+	hideAll($('#contact'));
+})
