28776969cc4e96707feff0e9b83ac6f2a0012c1f
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-06 15:26:35 -0500

    ngclass battle

--- controllers/work.js
+++ controllers/work.js
@@ -1,7 +1,16 @@
-mySite.controller('WorkCtrl', function($scope, $routeParams) {
+mySite.controller('WorkCtrl', function($scope, $routeParams, $location) {
+  if($location.$$path === '/') {
+    $scope.home = true;
+      console.log($scope.home)
+  } else {
+    $scope.home = false;
+      console.log($scope.home)
+  }
+
 	$scope.project = $routeParams.project;
   $scope.starWars = false;
   $scope.star = () => {
     $scope.starWars = !$scope.starWars;
   }
+
 })
